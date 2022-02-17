334d3a550eed009bf658c2d4276252198f2aa16d
eya-cfu <zaouieya@gmail.com>
2022-02-17 17:27:26 +0100

    workshop 3

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "gcp workshop 2" });
+  res.render("index", { title: "GDSC workshop 3" });
 });
 
 module.exports = router;
