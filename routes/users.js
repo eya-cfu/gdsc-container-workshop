773db154c856de88d315dce907d6f70cc9893c07
eya-cfu <zaouieya@gmail.com>
2022-02-17 19:11:16 +0100

    our users

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('respond with a resource. Woops looks like we have no users!');
 });
 
 module.exports = router;
