44dcf8a6367fbfe0f6db77cfc0f845969f9aa586
Eya_cfu <zaouieya@gmail.com>
2022-02-17 19:14:49 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "GDSC workshop 3" });
+  res.render("index", { title: "GDSC CI/CD workshop" });
 });
 
 module.exports = router;
